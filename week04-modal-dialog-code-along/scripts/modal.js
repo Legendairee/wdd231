a8ee24cba3e8ff31a6214e7027ace9d04d8b8dbc
legendairee <augustinelegend@gmail.com>
2026-09-21 22:49:20 +0100

    Multiple modal dialog practice activity

--- week04-modal-dialog-code-along/scripts/modal.js
+++ week04-modal-dialog-code-along/scripts/modal.js
@@ -1,12 +1,26 @@
-const openButton = document.querySelector("#openButton");
+const openButton1 = document.querySelector("#openButton1");
+const openButton2 = document.querySelector("#openButton2");
+const openButton3 = document.querySelector("#openButton3");
+
+
 const dialogBox = document.querySelector("#dialogBox");
+const dialogBoxText = document.querySelector("#dialogBox div");
 const closeButton = document.querySelector("#closeButton");
 
-openButton.addEventListener("click", () => {
+openButton1.addEventListener("click", () => {
+    dialogBoxText.innerHTML = "An Apple has 95 Calories"
+    dialogBox.showModal();
+});
+openButton2.addEventListener("click", () => {
+    dialogBoxText.innerHTML = "An Orange has 45 Calories"
+    dialogBox.showModal();
+});
+openButton3.addEventListener("click", () => {
+    dialogBoxText.innerHTML = "A Banana has 105 Calories"
     dialogBox.showModal();
 });
 
 closeButton.addEventListener("click", () => {
 
     dialogBox.close();
-});
+})
